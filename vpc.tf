6d35ae7cbdd656c9ab546b61b8101e77406580d5
DanielC <i7.danielcc@gmail.com>
2025-10-29 15:14:03 -0500

    Networking attpt 1

---
+++ vpc.tf
@@ -0,0 +1,72 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-vpc"
+  }
+}
+
+resource "aws_subnet" "publicA" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-subnet-public-a"
+  }
+}
+
+resource "aws_subnet" "publicB" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1b"
+
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-subnet-public-b"
+  }
+}
+
+resource "aws_subnet" "publicC" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-subnet-public-c"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-igw"
+  }
+}
+
+resource "aws_route_table" "routing" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-rt"
+  }
+}
+
+resource "aws_route_table_association" "associateA" {
+  subnet_id      = aws_subnet.publicA.id
+  route_table_id = aws_route_table.routing.id
+}
+
+resource "aws_route_table_association" "associateB" {
+  subnet_id      = aws_subnet.publicB.id
+  route_table_id = aws_route_table.routing.id
+}
+
+resource "aws_route_table_association" "associateC" {
+  subnet_id      = aws_subnet.publicC.id
+  route_table_id = aws_route_table.routing.id
+}
+
+resource "aws_route_table_association" "associateInternet" {
+  gateway_id     = aws_internet_gateway.gw.id
+  route_table_id = aws_route_table.routing.id
+}
