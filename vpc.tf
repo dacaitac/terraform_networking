931e8dbc114a5dd2b3cc861c3b5001c2fbe9d55f
DanielC <i7.danielcc@gmail.com>
2025-10-30 10:31:28 -0500

    Networking attpt 3

--- vpc.tf
+++ vpc.tf
@@ -1,51 +1,72 @@
-# Create VPC
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr
+  cidr_block = "10.10.0.0/16"
   tags = {
     Name = "cmtr-zt9dp1hp-01-vpc"
   }
 }
 
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
+resource "aws_subnet" "publicA" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-subnet-public-a"
+  }
+}
+
+resource "aws_subnet" "publicB" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.3.0/24"
+  availability_zone = "us-east-1b"
+
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-subnet-public-b"
+  }
+}
+
+resource "aws_subnet" "publicC" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.5.0/24"
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-subnet-public-c"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "cmtr-zt9dp1hp-01-igw"
+  }
+}
+
+resource "aws_route_table" "routing" {
   vpc_id = aws_vpc.main.id
   tags = {
-    Name = var.igw_name
+    Name = "cmtr-zt9dp1hp-01-rt"
   }
 }
 
-# Public Subnets
-resource "aws_subnet" "public" {
-  for_each = { for subnet in var.subnets : subnet.name => subnet }
-
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = each.value.cidr
-  availability_zone       = each.value.az
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = each.value.name
-  }
+resource "aws_route_table_association" "associateA" {
+  subnet_id      = aws_subnet.publicA.id
+  route_table_id = aws_route_table.routing.id
 }
 
-# Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = var.rt_name
-  }
+resource "aws_route_table_association" "associateB" {
+  subnet_id      = aws_subnet.publicB.id
+  route_table_id = aws_route_table.routing.id
 }
 
-# Route to Internet
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+resource "aws_route_table_association" "associateC" {
+  subnet_id      = aws_subnet.publicC.id
+  route_table_id = aws_route_table.routing.id
 }
 
-# Associate subnets to route table
-resource "aws_route_table_association" "public_assoc" {
-  for_each       = aws_subnet.public
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.public.id
+resource "aws_route_table_association" "associateInternet" {
+  gateway_id     = aws_internet_gateway.gw.id
+  route_table_id = aws_route_table.routing.id
 }
